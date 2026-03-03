9d4f42a9b6e4e9c9897363e96a6c82dc533dc4da
Mudugulla Pramod <pramodreddy0209@gmail.com>
2026-03-03 22:43:04 +0530

    Initial commit: add index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Convert markdown files into slide presentations in the browser
+ */
+
+function main() {
+  console.log("markdown-slide-presenter — running...");
+  // TODO: Implement core logic
+  console.log("Done!");
+}
+
+main();
